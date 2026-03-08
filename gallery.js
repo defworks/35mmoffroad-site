0f01cc371c3ef20903d8cfb1341dd682ea56e154
defworks <master.lr4x4@gmail.com>
2026-03-08 10:34:04 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,1 +1,28 @@
+const images = document.querySelectorAll(".gallery img")
+const lightbox = document.getElementById("lightbox")
+const lightboxImg = document.getElementById("lightbox-img")
+const closeBtn = document.querySelector(".close")
 
+images.forEach(img => {
+
+img.addEventListener("click", () => {
+
+lightbox.style.display = "flex"
+
+lightboxImg.src = img.src
+
+})
+
+})
+
+closeBtn.onclick = () => {
+
+lightbox.style.display = "none"
+
+}
+
+lightbox.onclick = () => {
+
+lightbox.style.display = "none"
+
+}
